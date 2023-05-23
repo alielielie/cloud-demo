082bea8dc3995ff2cbadf59574fe9f796fc5c341
zt <2916461184@qq.com>
2023-05-23 19:30:10 +0800

    cloud

---
+++ gateway/src/main/java/cn/itcast/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package cn.itcast.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @BelongsProject: cloud-demo
+ * @BelongsPackage: cn.itcast.gateway
+ * @Author: zt
+ * @CreateTime: 2023-05-19  16:18
+ * @Description:
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
